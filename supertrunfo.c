1510c2982af0f297de102aa2c07fc15f77bcb589
Duca77 <ducajulio66@yahoo.com.br>
2025-06-19 12:15:23 +0000

    Carta 1.

--- supertrunfo.c
+++ supertrunfo.c
@@ -9,18 +9,20 @@ int main(){
     char cidade[20];
     int populacao;
     float area;
-    float PIB;
+    float PIB; 
     int pontos_turisticos;
-
+    float densidade_populacional;
+    float PIB_per_capita;
+    
     printf("Digite o estado: ");
     scanf("%s", estado);
 // O estado é uma string, então usamos %s para ler 
     printf("Digite o codigo: ");
     scanf("%s", codigo);
-// O codigo é uma string composta por letras e números, então usamos %s para ler também 
+// O codigo é uma string, então usamos %s para ler 
     printf("Digite a cidade: ");
     scanf("%s", cidade);
-// A cidade é uma string, então usamos %s para ler do mesmo modo   
+// A cidade é uma string, então usamos %s para ler 
     printf("Digite a populacao: ");
     scanf("%d", &populacao);
 // A populacao é um inteiro, então usamos %d para ler 
@@ -29,10 +31,16 @@ int main(){
 // A area é um float, então usamos %f para ler
     printf("Digite o PIB: ");
     scanf("%f", &PIB);
-// O PIB é um float, então usamos %f para ler também 
+// O PIB é um float, então usamos %f para ler 
     printf("Digite os pontos turisticos: ");
     scanf("%d", &pontos_turisticos);
-// Os pontos turisticos são um inteiro, então usamos %d para ler também 
+// Os pontos turisticos são um inteiro, então usamos %d para ler 
+    printf("Digite a densidade populacional: ");
+    scanf("%f", &densidade_populacional);
+    
+    printf("Digite o PIB per capita: ");    
+    scanf("%f", &PIB_per_capita);
+    
     return 0;
     
      
